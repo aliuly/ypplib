5b09fc4b8fbba1a83707b5a7bc092b4b7abffbe7
Alejandro Liu <alejandrol@t-systems.com>
2024-07-09 18:32:21 +0200

    Simple clean-ups

---
+++ ypp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Run python using venv
+#
+set -euf
+(set -o pipefail >/dev/null 2>&1) && set -o pipefail || :
+
+mydir="$(dirname "$0")"
+venv="$mydir/.venv"
+sh "$mydir/setup.sh"
+. "$venv"/bin/activate
+export PYTHONPATH=${PYTHONPATH:-}:$mydir
+
+exec python3 -m ypp "$@"
+
